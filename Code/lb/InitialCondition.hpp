f42ab66553d73da3640d4e966fd56985fc822e4d
Titaporn F <s2554171@ed.ac.uk>
2024-07-29 09:53:21 +0100

    apply centreline IC to IC cal (built-tem)

--- Code/lb/InitialCondition.hpp
+++ Code/lb/InitialCondition.hpp
@@ -55,65 +55,59 @@ void EquilibriumInitialCondition::SetFs(geometry::FieldData* latDat, const net::
 template<class LatticeType>
 void CentrelineInitialCondition::SetFs(geometry::FieldData* latDat, const net::IOCommunicator& ioComms) const {
   distribn_t f_eq[LatticeType::NUMVECTORS];
-  LatticeType::CalculateFeq(density, mom_x, mom_y, mom_z, f_eq);
+  // LatticeType::CalculateFeq(1.0, 0.0, 0.0, 0.0, f_eq);
 
   // read centerline object
   // TODO: change to list of centerline coordinates, convert to lattice unit (builder)
-  const util::Vector3D<float> centerline_point1 = {0.000000,-0.000000,50.061935};
-  const util::Vector3D<float> centerline_point2 = {0.000000,0.000000,-37.801815};
+  const auto centerline_point1 = centrelineCoordinate[0];
+  const auto centerline_point2 = centrelineCoordinate.back();
 
-  const float pressure_point1 = 2.666440; // scalar
-  const float pressure_point2 = 0.323606;
+  const float pressure_point1 = pressureMagnitude[0]; // 0.000000;
+  const float pressure_point2 = pressureMagnitude.back(); // 0.000000;
+  std::cout << "pressure_point1: " << pressure_point1 << std::endl; // REMOVE
+  std::cout << "pressure_point2: " << pressure_point2 << std::endl; // REMOVE
 
-  const util::Vector3D<float> velocity_point1 = {0.000000,0.000000,0.266644};
-  const util::Vector3D<float> velocity_point2 = {0.000000,0.000000,0.266644};
+  const auto velocity_point1 = velocityCoordinate[0];
+  const auto velocity_point2 = velocityCoordinate.back();
+
+  const float radius_point1 = radiusDistance[0];
+  const float radius_point2 = radiusDistance.back();
 
   // TODO: Remove hardcode. Change to read from configuration
-  auto radius = 0.5;
   auto length = (centerline_point1 - centerline_point2).GetMagnitude();
-  configuration::FluidInfo fluidInfo;
-  double fluid_viscosity = fluidInfo.viscosity_Pas; // need to convert to lattice unit?
 
-  // TODO: Move inside the loop after the correct centerline point is obtain
-  const util::Vector3D<float> unit_vector = (centerline_point2 - centerline_point1).GetNormalised();
+  // TODO: Move inside the loop after the correct centerline point is obtained
+  const auto unit_vector = (centerline_point2 - centerline_point1).GetNormalised();
   // std::cout << "unit_vec" << unit_vector[0] << unit_vector[1] << unit_vector[2] << std::endl; // REMOVE
   
   for (site_t i = 0; i < latDat->GetDomain().GetLocalFluidSiteCount(); i++) {
 
     // obtain lattice coordinates
     auto site = latDat->GetSite(i);
-    const util::Vector3D<site_t> coordinates = site.GetGlobalSiteCoords();
+    const auto coordinates = site.GetGlobalSiteCoords();
 
     // TODO: Add calculation to obtain the closest centerline points (currently use the further end)
 
     // extrapolate from centerline properties to lattice properties
-    const util::Vector3D<float> centerline_lattice_vector = coordinates - centerline_point1;
+    const auto centerline_lattice_vector = coordinates - centerline_point1;
     auto projection_distance = Dot(centerline_lattice_vector, unit_vector);
-    const util::Vector3D<float>  perpendicular_vector = centerline_lattice_vector - projection_distance * unit_vector;
-    auto perpendicular_distance = perpendicular_vector.GetMagnitude(); 
-    // fix scalar P
-    auto lattice_pressure = pressure_point1 - (pressure_point1 - pressure_point2) * projection_distance / length;
-    // LatticeVelocity lattice_velocity = (pressure_point1 - pressure_point2) / (4 * fluid_viscosity * length) * (radius * radius - perpendicular_distance * perpendicular_distance);
-    LatticeVelocity lattice_velocity = velocity_point1 * (1 - (perpendicular_distance * perpendicular_distance) / (radius * radius));
+    const auto  perpendicular_vector = centerline_lattice_vector - projection_distance * unit_vector;
+    auto perpendicular_distance = perpendicular_vector.GetMagnitude();
 
-    // conversions TBC
+    LatticePressure lattice_pressure = pressure_point1 - (pressure_point1 - pressure_point2) * projection_distance / length;
+    LatticeVelocity lattice_velocity = velocity_point1 * (1 - (perpendicular_distance * perpendicular_distance) / (radius_point1 * radius_point1));
+
+    // convert velocity and pressure to density and momentum
     LatticeDensity density = lattice_pressure / Cs2;
-    // distribn_t mom_x = static_cast<distribn_t>(density * lattice_velocity[0]);
-    // distribn_t mom_y = static_cast<distribn_t>(density * lattice_velocity[1]);
-    // distribn_t mom_z = static_cast<distribn_t>(density * lattice_velocity[2]);
-
-    LatticeMomentum lattice_momentum;
-    lattice_momentum.x() = density * lattice_velocity.x();
-    lattice_momentum.y() = density * lattice_velocity.y();
-    lattice_momentum.z() = density * lattice_velocity.z();
-
-    // LatticeMomentum lattice_momentum = {density * lattice_velocity[0], density * lattice_velocity[1], density * lattice_velocity[2]};
-
-    std::cout << "mom_x: " << lattice_momentum.x() << " mom_y: " << lattice_momentum.y() << " mom_z: " << lattice_momentum.z() << std::endl;
-
+    LatticeMomentum lattice_momentum; // TODO: Simplify
+    // density * lattice_velocity
+    lattice_momentum = density * lattice_velocity;
+    // lattice_momentum.x() = density * lattice_velocity.x();
+    // lattice_momentum.y() = density * lattice_velocity.y();
+    // lattice_momentum.z() = density * lattice_velocity.z();
     
-    // should accept LatticeMomentum 3D
     LatticeType::CalculateFeq(density, lattice_momentum, f_eq);
+    // LatticeType::CalculateFeq(density, lattice_momentum.x(), lattice_momentum.y(), lattice_momentum.z() , f_eq);
 
     // get dist 
     distribn_t* f_old_p = this->GetFOld(latDat, i * LatticeType::NUMVECTORS);
